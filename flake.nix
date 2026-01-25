46fa2bcd7752ca2ba911e077d73b0fc574f7c9c9
Multirious <multirious@outlook.com>
2026-01-25 15:22:24 +0700

    Remove flake-utils dependency from flake.nix

--- flake.nix
+++ flake.nix
@@ -5,55 +5,67 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
   };
   outputs =
-    { nixpkgs, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          overlays = [ (import inputs.rust-overlay) ];
-          inherit system;
-        };
-        buildInputs = with pkgs; [
-          openssl
-          udev
-          alsa-lib-with-plugins
-          vulkan-loader
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr
-          libxkbcommon
-          wayland
-        ];
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-        ];
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-        stableRust = (
-          pkgs.rust-bin.stable."1.90.0".default.override {
-            extensions = [
-              "rust-analyzer"
-              "rust-src"
-            ];
-          }
-        );
-        nightlyRust = pkgs.rust-bin.selectLatestNightlyWith (
-          toolchain:
-          toolchain.default.override {
-            extensions = [
-              "rust-analyzer"
-              "rust-src"
-            ];
-          }
-        );
-      in
-      {
-        devShells = {
+    inputs:
+    let
+      forAllSystems =
+        function:
+        inputs.nixpkgs.lib.genAttrs
+          [
+            "x86_64-linux"
+          ]
+          (
+            system:
+            function (
+              import inputs.nixpkgs {
+                inherit system;
+                overlays = [
+                  (import inputs.rust-overlay)
+                ];
+              }
+            )
+          );
+    in
+    {
+      devShells = forAllSystems (
+        pkgs:
+        let
+          buildInputs = with pkgs; [
+            openssl
+            udev
+            alsa-lib-with-plugins
+            vulkan-loader
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+            libxkbcommon
+            wayland
+          ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          stableRust = (
+            pkgs.rust-bin.stable."1.90.0".default.override {
+              extensions = [
+                "rust-analyzer"
+                "rust-src"
+              ];
+            }
+          );
+          nightlyRust = pkgs.rust-bin.selectLatestNightlyWith (
+            toolchain:
+            toolchain.default.override {
+              extensions = [
+                "rust-analyzer"
+                "rust-src"
+              ];
+            }
+          );
+        in
+        {
           default = pkgs.mkShell {
             inherit nativeBuildInputs LD_LIBRARY_PATH;
             buildInputs = buildInputs ++ [ stableRust ];
@@ -62,7 +74,7 @@
             inherit nativeBuildInputs LD_LIBRARY_PATH;
             buildInputs = buildInputs ++ [ nightlyRust ];
           };
-        };
-      }
-    );
+        }
+      );
+    };
 }
