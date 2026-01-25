4ef84d074525a7f55fef49d81ab2734ede905399
Rabbival <87331993+Rabbival@users.noreply.github.com>
2026-01-25 17:47:04 +0200

    Revert "Merge branch 'main' into generic_time_steps"
    This reverts commit 19c3d97dcf509890b0b60f9cacd98f063512189f, reversing
    changes made to fcb9b21b748249751cded4eada5b37e9d33142ec.

--- flake.nix
+++ flake.nix
@@ -5,65 +5,55 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
   outputs =
-    inputs:
-    let
-      forAllSystems =
-        function:
-        inputs.nixpkgs.lib.genAttrs
-          [
-            "x86_64-linux"
-          ]
-          (
-            system:
-            function (
-              import inputs.nixpkgs {
-                inherit system;
-                overlays = [
-                  (import inputs.rust-overlay)
-                ];
-              }
-            )
-          );
-    in
-    {
-      devShells = forAllSystems (
-        pkgs:
-        let
-          buildInputs = with pkgs; [
-            openssl
-            udev
-            alsa-lib-with-plugins
-            vulkan-loader
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXrandr
-            libxkbcommon
-            wayland
-          ];
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-          stableRust = pkgs.rust-bin.stable.latest.default.override {
+    { nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          overlays = [ (import inputs.rust-overlay) ];
+          inherit system;
+        };
+        buildInputs = with pkgs; [
+          openssl
+          udev
+          alsa-lib-with-plugins
+          vulkan-loader
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          libxkbcommon
+          wayland
+        ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        stableRust = (
+          pkgs.rust-bin.stable."1.90.0".default.override {
             extensions = [
               "rust-analyzer"
               "rust-src"
             ];
-          };
-          nightlyRust = pkgs.rust-bin.selectLatestNightlyWith (
-            toolchain:
-            toolchain.default.override {
-              extensions = [
-                "rust-analyzer"
-                "rust-src"
-              ];
-            }
-          );
-        in
-        {
+          }
+        );
+        nightlyRust = pkgs.rust-bin.selectLatestNightlyWith (
+          toolchain:
+          toolchain.default.override {
+            extensions = [
+              "rust-analyzer"
+              "rust-src"
+            ];
+          }
+        );
+      in
+      {
+        devShells = {
           default = pkgs.mkShell {
             inherit nativeBuildInputs LD_LIBRARY_PATH;
             buildInputs = buildInputs ++ [ stableRust ];
@@ -72,7 +62,7 @@
             inherit nativeBuildInputs LD_LIBRARY_PATH;
             buildInputs = buildInputs ++ [ nightlyRust ];
           };
-        }
-      );
-    };
+        };
+      }
+    );
 }
